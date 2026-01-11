f79e5efeea112767028b82ce0f64e8f9c4eaee2e
pradumanrathod <pradumanr25@gmail.com>
2026-01-11 10:38:58 +0530

    authprovider changes

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import AuthContext from "./authContext";
+import AuthContext from "./AuthContext";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
